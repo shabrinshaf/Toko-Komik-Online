d9e194f9d1002cfa96b923eb1993e9a711fbc2a9
Shabrina Hikmah <shabrinahshaf@gmail.com>
2025-11-28 19:52:56 +0700

    Menambahkan seluruh folder komik beserta isinya

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import router from "./routes/userRoutes.js";
 
 const app = express();
 const PORT = 3000;
@@ -7,10 +8,6 @@ app.use(express.json());
 
  app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
-});
-
-app.get("/", (req, res) => {
-    res.send("Welcome to Online Store!");
 });
 
 app.use(router);
