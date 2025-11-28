7a7f29110fc6e6f63ea138238f1594fe99788177
Shabrina Hikmah <shabrinahshaf@gmail.com>
2025-11-28 18:07:40 +0700

    Menambahkan seluruh folder komik beserta isinya

---
+++ data.js
@@ -0,0 +1,36 @@
+let dataKomik = [
+  {
+    id: 1,
+    title: "Detective Conan",
+    author: "Gosho Aoyama",
+    stock: 25,
+    price: 35000,
+    genre: "Mystery"
+  },
+  {
+    id: 2,
+    title: "Noragami",
+    author: "Adachitoka",
+    stock: 15,
+    price: 42000,
+    genre: "Fantasy"
+  },
+  {
+    id: 3,
+    title: "Hunter x Hunter",
+    author: "Yoshihiro Togashi",
+    stock: 10,
+    price: 50000,
+    genre: "Action"
+  },
+  {
+    id: 4,
+    title: "Doraemon",
+    author: "Fujiko F. Fujio",
+    stock: 40,
+    price: 30000,
+    genre: "Comedy"
+  }
+];
+
+export default dataKomik;
--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-console.log("Hello, World!");
-console.log("This is the index.js file.");
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+ app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
+
+app.get("/", (req, res) => {
+    res.send("Welcome to Toko Komik Online!");
+});
+
+app.use(router);
